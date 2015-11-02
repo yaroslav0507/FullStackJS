b7cada160f61fbebc96a2408a0498a93ff80907b
Yaroslav Zaklinsky <yaroslav0507@gmail.com>
2015-11-03 00:12:41 +0200

    JS uglify + sourcemaps, CSS minify + sourcemaps, HTML minify

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -2,22 +2,37 @@
 
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+var sourcemaps = require('gulp-sourcemaps');
 
 /* Release tasks */
-gulp.task('buildJS', ['dev'], function(){
+gulp.task('buildJS', function(){
     return gulp.src('dist/**/*.js')
         .pipe(concat('app.js'))
+        .pipe(sourcemaps.init())
+        .pipe(uglify({mangle: false}))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('release'));
 });
 
-gulp.task('buildCSS', ['dev'], function(){
+gulp.task('buildCSS', function(){
+    var cssOption = {
+        compatibility: 'ie8',
+        s0: true
+    };
+
     return gulp.src('dist/**/*.css')
         .pipe(concat('app.css'))
+        .pipe(sourcemaps.init())
+        .pipe(minifyCss(cssOption))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('release'));
 });
 
 gulp.task('buildHTML', function(){
     return gulp.src('dist/index.html')
+        .pipe(minifyHTML())
         .pipe(gulp.dest('release'));
 });
 
