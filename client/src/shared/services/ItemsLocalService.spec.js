915586537f9c94675840971fef1452209d2cc33f
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-11-10 18:20:58 +0200

    StoreController.spec.js

--- client/src/shared/services/ItemsLocalService.spec.js
+++ client/src/shared/services/ItemsLocalService.spec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('service: ItemsLocalService', function(){
     var ItemsLocalService;
 
---
+++ client/src/store/StoreController.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+describe('StoreController', function(){
+
+   var sut,
+       cart,
+       categories,
+       CartService,
+       $state;
+
+   beforeEach(function(){
+       module('app');
+
+       $state = {
+           go: env.stub()
+       };
+
+       CartService.addToCart = env.stub();
+   });
+
+    beforeEach(inject(function(_$controller_,
+                               _cart_,
+                               _categories_,
+                               _CartService_,
+                               _$state_){
+
+        cart = _cart_;
+        categories = _categories_;
+        CartService = _CartService_;
+        $state = _$state_;
+
+        sut = _$controller_('StoreController', {
+            cart: cart,
+            categories: categories,
+            CartService: CartService,
+            $state: $state
+        })
+    }));
+
+   describe('@addToCart', function(){
+        beforeEach(function(){
+            var item = {};
+        });
+
+        it('should call CartService.addToCart with item', function(){
+            CartService.addToCart.should.have.been.calledWith(item);
+        });
+   });
+
+});
