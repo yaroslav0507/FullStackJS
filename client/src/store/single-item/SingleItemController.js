269138f67f0ee7a23aa99b50d46e128884f66edd
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-11-19 15:43:04 +0200

    [Store] Like, Share item buttons.

--- client/src/store/single-item/SingleItemController.js
+++ client/src/store/single-item/SingleItemController.js
@@ -29,11 +29,6 @@
             scrollZoom : true
         };
 
-        vm.tabs = [
-            { title:'Dynamic Title 1', content:'Dynamic content 1' },
-            { title:'Dynamic Title 2', content:'Dynamic content 2' }
-        ];
-
         vm.item.qty = 1;
 
         function addToCart(){
