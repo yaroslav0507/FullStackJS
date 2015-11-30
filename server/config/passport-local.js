5744c04e3fa5f20c83b7b8a191221be0a09f9234
Yaroslav Zaklinskyi <Yaroslav_Zaklinskyi@epam.com>
2015-11-30 15:36:39 +0200

    Merge branch 'angular-loading-bar'
    Conflicts:
    	client/src/app/loadingBar.js

---
+++ server/config/passport-local.js
@@ -0,0 +1,19 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+   function(username, password, done){
+       User.findOne({ username: username }, function(err, user){
+           if (err) { return done(err); }
+           if (!user) {
+               return done(null, false, { message: 'Incorrect username.' });
+           }
+           if (!user.validPassword(password)){
+               return done(null, false, { message: 'Incorrect Password' });
+           }
+           return done(null, user);
+       });
+   }
+));
